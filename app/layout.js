af7c5cee33c3aabf56774c4897b89a8d72aa0fe8
DSSHike <yourdesigner456@gmail.com>
2023-12-27 15:23:46 +0530

    personal home page added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Script from 'next/script'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +11,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      
+      
+    <Script src="assets/vendor/purecounter/purecounter_vanilla.js" />
+  <Script src="assets/vendor/aos/aos.js" />
+  <Script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js" />
+  <Script src="assets/vendor/glightbox/js/glightbox.min.js" />
+  <Script src="assets/vendor/isotope-layout/isotope.pkgd.min.js" />
+  <Script src="assets/vendor/swiper/swiper-bundle.min.js" />
+  <Script src="assets/vendor/typed.js/typed.umd.js" />
+  <Script src="assets/vendor/waypoints/noframework.waypoints.js" />
+  <Script src="assets/vendor/php-email-form/validate.js" />
+
+ 
+  <Script src="assets/js/main.js" />
+
+      <Script>
+        alert ("hello guys")
+      </Script>
+      </body>
     </html>
   )
 }
